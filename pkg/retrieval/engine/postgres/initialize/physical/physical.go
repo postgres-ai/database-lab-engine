71eebb70ea2ed305851f2c1151729ff654e1138b
akartasov <agneum@gmail.com>
2020-08-13 16:18:12 +0700

    feat: start with the default postgresql.conf (#149)

--- pkg/retrieval/engine/postgres/initialize/physical/physical.go
+++ pkg/retrieval/engine/postgres/initialize/physical/physical.go
@@ -29,8 +29,10 @@ import (
 	"gitlab.com/postgres-ai/database-lab/pkg/retrieval/dbmarker"
 	"gitlab.com/postgres-ai/database-lab/pkg/retrieval/engine/postgres/initialize/tools"
 	"gitlab.com/postgres-ai/database-lab/pkg/retrieval/engine/postgres/initialize/tools/defaults"
+	"gitlab.com/postgres-ai/database-lab/pkg/retrieval/engine/postgres/initialize/tools/fs"
 	"gitlab.com/postgres-ai/database-lab/pkg/retrieval/options"
 	"gitlab.com/postgres-ai/database-lab/pkg/services/provision/databases/postgres/configuration"
+	"gitlab.com/postgres-ai/database-lab/pkg/util"
 )
 
 const (
@@ -41,6 +43,8 @@ const (
 	restoreContainerPath = "/var/lib/postgresql/dblabdata"
 
 	readyLogLine = "database system is ready to accept"
+
+	defaultPgConfigsDir = "default"
 )
 
 // RestoreJob describes a job for physical restoring.
@@ -184,6 +188,16 @@ func (r *RestoreJob) Run(ctx context.Context) error {
 	pgVersion, err := tools.DetectPGVersion(r.globalCfg.DataDir)
 	if err != nil {
 		return errors.Wrap(err, "failed to detect the Postgres version")
+	}
+
+	// Prepare configuration files.
+	sourceConfigDir, err := util.GetConfigPath(path.Join(defaultPgConfigsDir, pgVersion))
+	if err != nil {
+		return errors.Wrap(err, "cannot get path to default configs")
+	}
+
+	if err := fs.CopyDirectoryContent(sourceConfigDir, r.globalCfg.DataDir); err != nil {
+		return errors.Wrap(err, "failed to set default configuration files")
 	}
 
 	if err := configuration.Run(r.globalCfg.DataDir); err != nil {
