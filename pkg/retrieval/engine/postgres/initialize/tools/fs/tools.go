314bd60e013361bc944dcdca9c508dfd7df7b49a
Artyom Kartasov <agneum@gmail.com>
2020-08-14 02:51:50 +0000

    feat: start with the default postgresql.conf (#149)

---
+++ pkg/retrieval/engine/postgres/initialize/tools/fs/tools.go
@@ -0,0 +1,59 @@
+/*
+2020 © Postgres.ai
+*/
+
+// Package fs provides tools for working with the filesystem.
+package fs
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// CopyDirectoryContent copies all files from one directory to another.
+func CopyDirectoryContent(sourceDir, dataDir string) error {
+	entries, err := ioutil.ReadDir(sourceDir)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		sourcePath := filepath.Join(sourceDir, entry.Name())
+		destPath := filepath.Join(dataDir, entry.Name())
+
+		if err := copyFile(sourcePath, destPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copyFile(sourceFilename, destinationFilename string) error {
+	dst, err := os.Create(destinationFilename)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = dst.Close() }()
+
+	src, err := os.Open(sourceFilename)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = src.Close() }()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
