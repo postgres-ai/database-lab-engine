530b1ed57f6a5034f26bab3d648d3b62e62a5feb
Dmitry Fomin <dmitry@postgres.ai>
2021-06-18 18:49:25 +0000

    added envoy

--- packer/install-prereqs.sh
+++ packer/install-prereqs.sh
@@ -1,25 +1,31 @@
 #/!bin/bash
 
 set -euxo pipefail
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee /etc/apt/sources.list.d/pgdg.list 
 
-sudo apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
   apt-transport-https \
   ca-certificates \
   gnupg-agent \
+  python3-software-properties \
   software-properties-common \
   curl \
-  postgresql-client  
-#  postgresql-contrib
+  gnupg2 \
+  postgresql-client-13  
 
-# ZFS
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+#install envoy
+curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
+sudo apt update && sudo apt-get install -y  getenvoy-envoy
 
-# docker
+#install docker 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update && sudo apt-get install -y \
   docker-ce \
   docker-ce-cli \
@@ -27,3 +33,4 @@ sudo apt-get update && sudo apt-get install -y \
   zfsutils-linux
 
 sudo docker pull  postgresai/dblab-server:2.3-latest
+
