6fd898e76e7cbbe3965118bcb6dbaa1fda9b95f3
Artyom Kartasov <agneum@gmail.com>
2021-12-09 02:39:20 +0000

    fix release 3.0.0-rc.1 issues: * remove the host parameter from the server configuration since it is always runs inside a Docker container * describe the meaning of empty `verificationToken` in configs

---
+++ pkg/util/engine/engine.go
@@ -0,0 +1,9 @@
+/*
+2021 © Postgres.ai
+*/
+
+// Package engine contains tools.
+package engine
+
+// DefaultListenerHost defines the default host of an HTTP listener.
+const DefaultListenerHost = "0.0.0.0"
