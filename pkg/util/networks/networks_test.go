d7cabadfca7905d534cb5260fc82fd67e5cd0000
Artyom Kartasov <agneum@gmail.com>
2022-02-07 08:58:37 +0000

    fix: skip connection if the DLE container is already connected to an internal network

--- pkg/util/networks/networks_test.go
+++ pkg/util/networks/networks_test.go
@@ -7,6 +7,7 @@ package networks
 import (
 	"testing"
 
+	"github.com/docker/docker/api/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,5 +16,32 @@ func TestInternalNetworks(t *testing.T) {
 		instanceID := "testInstanceID"
 
 		assert.Equal(t, "dle_network_testInstanceID", getNetworkName(instanceID))
+	})
+}
+
+func TestIfContainerConnected(t *testing.T) {
+	t.Run("test if container connected", func(t *testing.T) {
+		resource := types.NetworkResource{
+			Containers: map[string]types.EndpointResource{
+				"testID": {Name: "test_server"},
+			},
+		}
+		testCases := []struct {
+			containerName string
+			result        bool
+		}{
+			{
+				containerName: "test_server",
+				result:        true,
+			},
+			{
+				containerName: "not_connected_server",
+				result:        false,
+			},
+		}
+
+		for _, tc := range testCases {
+			assert.Equal(t, tc.result, hasContainerConnected(resource, tc.containerName))
+		}
 	})
 }
