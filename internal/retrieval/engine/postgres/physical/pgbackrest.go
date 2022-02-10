f21929faebcc8890e9b7c1d4a01ec117295806ec
Nikolay Devxx <ndevxx@gmail.com>
2022-02-10 05:42:00 +0000

    feat(engine): add data source: pgBackRest (#265)

---
+++ internal/retrieval/engine/postgres/physical/pgbackrest.go
@@ -0,0 +1,56 @@
+/*
+2020 © Postgres.ai
+*/
+
+package physical
+
+import (
+	"fmt"
+
+	"gitlab.com/postgres-ai/database-lab/v3/internal/retrieval/engine/postgres/tools/defaults"
+)
+
+const (
+	pgbackrestTool = "pgbackrest"
+)
+
+// pgbackrest defines a pgBackRest as an archival restoration tool.
+type pgbackrest struct {
+	pgDataDir string
+	options   pgbackrestOptions
+}
+
+type pgbackrestOptions struct {
+	Stanza    string `yaml:"stanza"`
+	ForceInit bool   `yaml:"forceInit"`
+}
+
+func newPgBackRest(pgDataDir string, options pgbackrestOptions) *pgbackrest {
+	return &pgbackrest{
+		pgDataDir: pgDataDir,
+		options:   options,
+	}
+}
+
+// GetRestoreCommand returns a command to restore data.
+func (p *pgbackrest) GetRestoreCommand() string {
+	if p.options.ForceInit {
+		return fmt.Sprintf("sudo -Eu postgres pgbackrest --delta --type=standby --pg1-path=%s --stanza=%s restore", p.pgDataDir, p.options.Stanza)
+	}
+
+	return fmt.Sprintf("sudo -Eu postgres pgbackrest --type=standby --pg1-path=%s --stanza=%s restore", p.pgDataDir, p.options.Stanza)
+}
+
+// GetRecoveryConfig returns a recovery config to restore data.
+func (p *pgbackrest) GetRecoveryConfig(pgVersion float64) map[string]string {
+	recoveryCfg := map[string]string{
+		"restore_command": fmt.Sprintf("pgbackrest --pg1-path=%s --stanza=%s archive-get %%f %%p", p.pgDataDir, p.options.Stanza),
+	}
+
+	if pgVersion < defaults.PGVersion12 {
+		recoveryCfg["standby_mode"] = "on"
+		recoveryCfg["recovery_target_timeline"] = "latest"
+	}
+
+	return recoveryCfg
+}
---
+++ internal/retrieval/engine/postgres/physical/pgbackrest_test.go
@@ -0,0 +1,38 @@
+package physical
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPgBackRestRecoveryConfig(t *testing.T) {
+	pgbackrest := newPgBackRest("dataDir", pgbackrestOptions{Stanza: "stanzaName"})
+
+	recoveryConfig := pgbackrest.GetRecoveryConfig(11.7)
+	expectedResponse11 := map[string]string{
+		"restore_command":          "pgbackrest --pg1-path=dataDir --stanza=stanzaName archive-get %f %p",
+		"standby_mode":             "on",
+		"recovery_target_timeline": "latest",
+	}
+	assert.Equal(t, expectedResponse11, recoveryConfig)
+
+	recoveryConfig = pgbackrest.GetRecoveryConfig(12.3)
+	expectedResponse12 := map[string]string{
+		"restore_command": "pgbackrest --pg1-path=dataDir --stanza=stanzaName archive-get %f %p",
+	}
+	assert.Equal(t, expectedResponse12, recoveryConfig)
+}
+
+func TestPgBackRestRestoreCommand(t *testing.T) {
+	pgbackrest := newPgBackRest("dataDir", pgbackrestOptions{Stanza: "stanzaName"})
+
+	restoreCmd := pgbackrest.GetRestoreCommand()
+	expectedResponse := "sudo -Eu postgres pgbackrest --type=standby --pg1-path=dataDir --stanza=stanzaName restore"
+	assert.Equal(t, expectedResponse, restoreCmd)
+
+	pgbackrest.options.ForceInit = true
+	restoreCmd = pgbackrest.GetRestoreCommand()
+	expectedResponse = "sudo -Eu postgres pgbackrest --delta --type=standby --pg1-path=dataDir --stanza=stanzaName restore"
+	assert.Equal(t, expectedResponse, restoreCmd)
+}
