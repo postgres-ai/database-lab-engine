d8b14e8420b258d0c0ee72cb17d649cb8199b1f2
Dmitry Fomin <dmitry@postgres.ai>
2022-05-11 18:08:21 +0000

    add jq package to DLE AMI

--- packer/install-prereqs.sh
+++ packer/install-prereqs.sh
@@ -50,3 +50,6 @@ sudo apt install s3fs
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
 sudo add-apt-repository ppa:rmescandon/yq
 sudo apt update && sudo apt install yq -y
+
+#install jq
+sudo apt install jq -y
