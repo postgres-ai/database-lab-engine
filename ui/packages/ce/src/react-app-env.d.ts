5c9e2a12ad54b366061af6a44e0d82848d0c2431
Artyom Kartasov <agneum@gmail.com>
2022-09-28 09:18:29 +0000

    fix(ui): enable logs using websockets for CloudFormation installations

--- ui/packages/ce/src/react-app-env.d.ts
+++ ui/packages/ce/src/react-app-env.d.ts
@@ -4,6 +4,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly REACT_APP_API_URL_PREFIX?: string
+    readonly REACT_APP_WS_URL_PREFIX?: string
     readonly BUILD_TIMESTAMP: number
   }
 }
