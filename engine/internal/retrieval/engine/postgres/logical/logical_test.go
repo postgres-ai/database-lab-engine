a92e01bf2590fc04d63261410c3f0f1f8c5281f1
akartasov <agneum@gmail.com>
2023-08-03 09:07:46 +0700

    fix: trim slashes before comparing mountpoints (#524)

---
+++ engine/internal/retrieval/engine/postgres/logical/logical_test.go
@@ -0,0 +1,46 @@
+package logical
+
+import (
+	"testing"
+
+	"github.com/docker/docker/api/types/mount"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsAlreadyMounted(t *testing.T) {
+	testCases := []struct {
+		source         []mount.Mount
+		dumpLocation   string
+		expectedResult bool
+	}{
+		{
+			source:         []mount.Mount{},
+			dumpLocation:   "/var/lib/dblab/pool/dump",
+			expectedResult: false,
+		},
+		{
+			source:         []mount.Mount{{Source: "/var/lib/dblab/pool/dump/"}},
+			dumpLocation:   "/var/lib/dblab/pool/dump/",
+			expectedResult: true,
+		},
+		{
+			source:         []mount.Mount{{Source: "/var/lib/dblab/pool/dump"}},
+			dumpLocation:   "/var/lib/dblab/pool/dump/",
+			expectedResult: true,
+		},
+		{
+			source:         []mount.Mount{{Source: "/var/lib/dblab/pool/dump/"}},
+			dumpLocation:   "/var/lib/dblab/pool/dump",
+			expectedResult: true,
+		},
+		{
+			source:         []mount.Mount{{Source: "/var/lib/dblab/pool/dump"}},
+			dumpLocation:   "/var/lib/dblab/new_pool/dump",
+			expectedResult: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, isAlreadyMounted(tc.source, tc.dumpLocation), tc.expectedResult)
+	}
+}
