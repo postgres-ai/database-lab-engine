f1246b0fa436cc93c7ea12954b23c8d770fb3511
Vitaliy Kukharik <vitaliy@postgres.ai>
2024-01-26 18:27:12 +0000

    Platform (UI): Update tag (v1.0) for dle-se-ansible image, adjust e2e tests

--- ui/packages/ce/cypress/e2e/tabs.cy.js
+++ ui/packages/ce/cypress/e2e/tabs.cy.js
@@ -1,45 +1,61 @@
 /* eslint-disable no-undef */
 
-describe('Configuration tab', () => {
-  before(() => {
-    // Set the token in local storage
-    cy.window().then((win) => {
-      if (!win.localStorage.getItem('token')) {
-        win.localStorage.setItem('token', 'demo-token')
-      }
-    })
+Cypress.on('uncaught:exception', () => {
+  return false
+})
+
+// Function to set up intercepts for the requests
+function setupIntercepts() {
+  cy.intercept('GET', '/healthz*', {
+    statusCode: 200,
+    body: {
+      edition: 'standard',
+    },
   })
 
-  it('should have token in local storage', () => {
-    // Check if the token exists in local storage
-    cy.window()
-      .should('have.property', 'localStorage')
-      .and('have.property', 'token', 'demo-token')
+  cy.intercept('GET', '/instance/retrieval*', {
+    statusCode: 200,
+    body: {
+      status: 'inactive',
+    },
+  })
+
+  cy.intercept('GET', '/status*', {
+    statusCode: 200,
+    body: {
+      status: {
+        code: 'OK',
+        message: 'Instance is ready',
+      },
+      pools: [],
+      cloning: {
+        clones: [],
+      },
+      retrieving: {
+        status: 'inactive',
+      },
+    },
+  })
+}
+
+describe('Configuration tab', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    setupIntercepts()
   })
 
   it('should have "Configuration" tab with form inputs', () => {
-    // Visit the page
-    cy.visit('/', {
-      retryOnStatusCodeFailure: true,
-      onLoad: () => {
-        // Click on the "Configuration" tab
-        cy.get('.MuiTabs-flexContainer').contains('Configuration').click({
-          force: true,
-        })
-
-        // Check for elements on the "Configuration" tab
-        cy.get('input[type="text"]').should('exist')
-        cy.get('input[type="checkbox"]').should('exist')
-        cy.get('button[type="button"]').should('exist')
-
-        // Click on the "Cancel" button within the "Configuration" tab
-        cy.get('button').contains('Cancel').click({
-          force: true,
-        })
-
-        // Check if the URL has changed to "/instance"
-        cy.url().should('eq', Cypress.config().baseUrl + '/instance')
-      },
+    cy.get('.MuiTabs-flexContainer').contains('Configuration').click({
+      force: true,
     })
+
+    cy.get('input[type="text"]').should('exist')
+    cy.get('input[type="checkbox"]').should('exist')
+    cy.get('button[type="button"]').should('exist')
+
+    cy.get('button').contains('Cancel').click({
+      force: true,
+    })
+    cy.url().should('eq', Cypress.config().baseUrl + '/instance')
   })
 })
