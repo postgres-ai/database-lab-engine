3ac0a9b1d05daad0ecbfcd49c762444345210d65
Nikolay Samokhvalov <nik@postgres.ai>
2024-03-22 17:35:24 +0000

    Use 2345 as server's API port in UI connection snippet

--- ui/packages/shared/pages/Instance/Info/Connection/ConnectModal/Content/utils.ts
+++ ui/packages/shared/pages/Instance/Info/Connection/ConnectModal/Content/utils.ts
@@ -15,7 +15,9 @@ export const getSshPortForwardingCommand = (instance: Instance) => {
     // Parse the URL to get the port
     const url = new URL(instance.url as string)
     const port = url.port || '2345'
-    return `ssh -NTML ${port}:localhost:${port} ${instance.sshServerUrl} -i ~/.ssh/id_rsa`
+    // Here we hard-code the API port on the server (2345)- this is a requirement now
+    // for all DBLab instances working via tunnel, per decision made (NIkolayS 2024-05-22)
+    return `ssh -NTML ${port}:localhost:2345 ${instance.sshServerUrl} -i ~/.ssh/id_rsa`
   } else {
     return null
   }
