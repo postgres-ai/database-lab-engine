9cec7f6235af87c200fd434e8d70ccdd037f54fb
Bogdan Tsechoev <bogdan@postgres.ai>
2024-07-09 16:10:42 +0000

    Bot UI: Hide Intercom widget on Bot page

--- ui/packages/platform/src/pages/Bot/BotWrapper.tsx
+++ ui/packages/platform/src/pages/Bot/BotWrapper.tsx
@@ -2,6 +2,7 @@ import { BotPage } from "./index";
 import {RouteComponentProps} from "react-router";
 import {AlertSnackbarProvider} from "@postgres.ai/shared/components/AlertSnackbar/useAlertSnackbar";
 import { AiBotProvider } from "./hooks";
+import { useHideIntercom } from "../../hooks/useHideIntercom";
 
 export interface BotWrapperProps {
   envData: {
@@ -24,6 +25,7 @@ export interface BotWrapperProps {
 
 
 export const BotWrapper = (props: BotWrapperProps) => {
+  useHideIntercom();
   return (
     <AlertSnackbarProvider>
       <AiBotProvider args={{ threadId: props.match.params.threadId, orgId: props.orgData.id }}>
